8aa479cab97b1941eff2917d3a2b1cd5de661a05
penicillin0 <ayahitonakamura@gmail.com>
2023-05-06 16:26:12 +0900

    update color

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,25 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#fff";
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
+    text: "#000",
+    background: "#fff",
+    darkGrey: "#444",
+    grey: "#888",
+    lightGrey: "#ccc",
+    tint: "#2f95dc",
+    tabIconDefault: "#ccc",
+    tabIconSelected: "#2f95dc",
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: "#fff",
+    background: "#000",
+    darkGrey: "#444",
+    grey: "#888",
+    lightGrey: "#ccc",
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorDark,
   },
 };
